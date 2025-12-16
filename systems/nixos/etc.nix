0ac9da7833ba73208b857aa4b4dfebbb34bd2865
jarrett <jarrett@freeformsoftware.dev>
2025-12-16 12:47:42 -0600

    Add MITMproxy

--- systems/nixos/etc.nix
+++ systems/nixos/etc.nix
@@ -1,3 +1,8 @@
 {
-      networking.hostName = "nixos";
+  networking.hostName = "nixos";
+
+  # Trust mitmproxy CA certificate for debugging HTTP/HTTPS traffic
+  security.pki.certificateFiles = [
+    ../../certs/nixos-mitmproxy-ca-cert.pem
+  ];
 }

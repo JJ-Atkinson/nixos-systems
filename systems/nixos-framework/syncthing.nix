35cc455b12661e692514ac8a54c14ce836b4323d
jarrett <jarrett@freeformsoftware.dev>
2025-12-30 21:09:16 -0600

    Remove syncthing settings which seem to be breaking options

--- systems/nixos-framework/syncthing.nix
+++ systems/nixos-framework/syncthing.nix
@@ -5,13 +5,5 @@
     user = "jarrett";
 
     dataDir = "/home/jarrett/syncthing-datadir";
-
-    settings.options = {
-      # Don't limit bandwidth on LAN connections
-      limitBandwidthInLan = false;
-
-      # Allow more folders to sync concurrently (0 = unlimited)
-      maxFolderConcurrency = 0;
-    };
   };
 }

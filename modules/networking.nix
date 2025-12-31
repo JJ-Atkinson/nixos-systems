15ef34b1c900985d975b5250e2304820b720db4d
jarrett <jarrett@freeformsoftware.dev>
2025-12-30 20:47:52 -0600

    Update some perf tweaks to syncthing

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,3 +1,9 @@
 {lib, ...} : {
     networking.useDHCP = lib.mkDefault true;
+
+    # Increase UDP buffer sizes for better performance with Syncthing/QUIC
+    boot.kernel.sysctl = {
+      "net.core.rmem_max" = 15000000;  # 15 MB receive buffer
+      "net.core.wmem_max" = 15000000;  # 15 MB send buffer
+    };
 }
--- systems/nixos/syncthing.nix
+++ systems/nixos/syncthing.nix
@@ -5,5 +5,13 @@
     user = "jarrett";
 
     dataDir = "/home/jarrett/syncthing-datadir";
+
+    settings.options = {
+      # Don't limit bandwidth on LAN connections
+      limitBandwidthInLan = false;
+
+      # Allow more folders to sync concurrently (0 = unlimited)
+      maxFolderConcurrency = 0;
+    };
   };
 }
